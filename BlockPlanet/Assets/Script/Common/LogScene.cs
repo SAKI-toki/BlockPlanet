e08a8cacfad73122e90f3c51071477db17ba996e
saki_bloom <toki.ryuka.senriyama@gmail.com>
2019-06-26 16:11:03 +0900

    debugを追加

---
+++ BlockPlanet/Assets/Script/Common/LogScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogScene : MonoBehaviour
+{
+    string currentSceneName;
+    string prevSceneName = "";
+    void Update()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        if (prevSceneName != currentSceneName)
+        {
+            Debug.Log(currentSceneName);
+            prevSceneName = currentSceneName;
+        }
+    }
+}
